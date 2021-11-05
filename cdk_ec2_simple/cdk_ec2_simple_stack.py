a46be85d0de837ce76b138799a4368676a8e3728
mirkenstein <mirkenstein@gmail.com>
2021-11-05 12:23:05 -0500

    Deployment complete.

--- cdk_ec2_simple/cdk_ec2_simple_stack.py
+++ cdk_ec2_simple/cdk_ec2_simple_stack.py
@@ -1,5 +1,5 @@
 from aws_cdk import core as cdk
-
+from aws_cdk import aws_ec2 as ec2
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
@@ -13,3 +13,23 @@ class CdkEc2SimpleStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        #Get Default VPC
+        vpc = ec2.Vpc.from_lookup(self, "VPC",
+                                  # This imports the default VPC but you can also
+                                  # specify a 'vpcName' or 'tags'.
+                                  is_default=True
+                                  )
+        my_security_group = ec2.SecurityGroup(self, "SecurityGroup",
+                                              vpc=vpc,
+                                              description="Allow ssh access to ec2 instances",
+                                              allow_all_outbound=True
+                                              )
+        my_security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "allow ssh access from the world")
+
+        #Launch EC2
+        my_ec2=ec2.Instance(self, "my_ec2",
+                            vpc=vpc,
+                            machine_image=ec2.MachineImage.latest_amazon_linux(),
+                            instance_type=ec2.InstanceType("t2.micro"),
+                            security_group=my_security_group,
+        )
